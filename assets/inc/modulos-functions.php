b55b58fcc84df22f4ab9046c59a23d355804fea7
jesus11003 <barrosjesus532@gmail.com>
2023-05-29 18:24:05 -0400

    cambio de ruta modulo functions
    cambio de ruta modulo functions

--- assets/inc/modulos-functions.php
+++ assets/inc/modulos-functions.php
@@ -1,2 +1,2 @@
 <?php
-include get_template_directory() . '/assets/modulos/modulo-musicas/core-modulo-musicas.php';
+include get_template_directory() . '/assets/modulos/modulo-productos/loop-productos.php';
