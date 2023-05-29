5845188c045bd961641219012883fcaa93b928fe
jesus11003 <barrosjesus532@gmail.com>
2023-05-29 17:21:16 -0400

    cambio nombre
    cambio nombre a loop-productos.php

--- assets/modulos/modulo-productos/loop-productos.php
+++ assets/modulos/modulo-productos/loop-productos.php
@@ -18,16 +18,19 @@ incrustar_hoja_estilos_comision();
             $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
             $post_per_page = -1; // -1 shows all posts
             $args = array(
-                'post_type' => 'autos',
+                'post_type' => 'product',
                 'orderby' => 'date',
                 'order' => 'ASC',
                 'paged' => $paged,
-                'posts_per_page' => $post_per_page
+                'posts_per_page' => $post_per_page,
             );
             $wp_query = new WP_Query($args);
     if (have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-
-       <h1> <?php echo get_the_title();?></h1>
+       <div class="card col-12 col-md-4">
+       <h1><?php echo get_the_title();?></h1>
+       <?php ecommerce_post_tumbnail();?>
+       <a href="<?php the_permalink();?>">ver mas</a>
+       </div>
 
 
 
